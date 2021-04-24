62129202298e9395207606bbfc897212bae4c7df
Mohamedelbeshlawy <mohamedelbeshlawyy@gmail.com>
2021-04-24 14:15:32 +0200

    fetching data

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,30 @@
-
 import './App.css';
-import {useState } from "react"
+import {useState, useEffect } from "react"
+
+
+
 
 function App() {
 
-  const [posts, setPosts] = useState([
-    {
-      userId: 1,
-      id: 1,
-      title: "sunt aut facere repellat provident occaecati excepturi optio reprehenderit",
-      body: "quia et suscipit suscipit recusandae consequuntur expedita et cum reprehenderit molestiae ut ut quas totam nostrum rerum est autem sunt rem eveniet architecto"
-      }
-  ])
+  const [posts, setPosts] = useState([])
+
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/posts')
+    .then(response => response.json())
+    .then(json => setPosts(json))
+  }, [])
+  
   return (
     <div className="App">
       <h1>User Posts</h1>
 
-      { posts.map((elem) => {
-        console.log(elem);
-       return <ul key={elem.userId}> <li> {elem.userId}</li> <li> { elem.body }</li>  </ul>
+      { posts.map((post) => {
+        console.log(post);
+       return <ul key={post.userId}> 
+       <li> {post.userId}</li>
+        <li>{post.title}</li>
+        <li> { post.body }</li> 
+        </ul>
       }) }
     </div>
   );

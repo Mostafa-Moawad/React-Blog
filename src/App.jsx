2c664f0044846dc1b86b64ea35bf26042cf42a4d
Mohamedelbeshlawy <mohamedelbeshlawyy@gmail.com>
2021-04-25 16:32:40 +0200

    Editing in app.js

--- src/App.jsx
+++ src/App.jsx
@@ -6,49 +6,49 @@ import {UserInput} from "./components/UserInput"
 import {CommentItem} from "./components/CommentItem"
 import PostList from './components/PostList';
 import { Login2 } from './components/Login2';
-
+import { navigate, Router } from "@reach/router";
 
 
 function App() {
 
   
   const [posts, setPosts] = useState([])
-
-
-  const handlePostList = (user_id) => {
-    fetch(`https://jsonplaceholder.typicode.com/users/${user_id}/posts`)
-    .then(response => response.json())
-    .then(json => setPosts(json))
-  }
+  const [authUser, setAuthUser] = userState([])
   
   const handleLogin = (userEmail,userName) => {
     fetch(`https://jsonplaceholder.typicode.com/users?username=${userName}&email=${userEmail}`)
     .then(response => response.json())
     .then(fetchedUser => {
       if(fetchedUser.length>0)
-      {
-          console.log(fetchedUser);  
-          // navigate("/")
-
+      {   
+          setAuthUser(fetchedUser)
+          console.log(fetchedUser[0].id + " userid");  
+          navigate("/")
       }
       else{
         console.log("error while routing");
-        // navigate("/login")
+        navigate("/login")
       }
 
     })
   }
+
+  const handlePostList = (user_id) => {
+    fetch(`https://jsonplaceholder.typicode.com/users/${authUser[0].id}/posts`)
+    .then(response => response.json())
+    .then(json => setPosts(json))
+  }
   
-
-
   return (
-    <div className="App">
-      <h1>User Posts</h1>
-      {/* <UserInput handlePostList = {handlePostList} /> */}
-      <Login2 handleLogin={handleLogin} /> 
-      <PostList posts= {posts} />
-      
-    </div>
+    <Router>
+      <Login2 path="/login" handleLogin={handleLogin} >
+        <div className="App">
+        <h1>User Posts</h1>
+        {/* <UserInput handlePostList = {handlePostList} /> */}
+        <PostList posts= {posts} handlePostList={handlePostList} />
+        </div>
+      </Login2>
+    </Router>
   );
 }
 
